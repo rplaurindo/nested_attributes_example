d449d5e3ac5d40f2a17d580ce3105ef066d71a68
Rafael Laurindo <rplaurindo>
2016-05-01 03:42:14 -0300

    add iterator to juice ingredients in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :juices, only: [:new, :index]
   get "/juices/ingredients/new" => "juices#new_ingredient", as: :new_juice_ingredient
-  get "/juices/edit"            => "juices#edit", as: :edit_juice
+  get "/juices/edit"            => "juices#edit"
 
   root to: "juices#edit"
 end
